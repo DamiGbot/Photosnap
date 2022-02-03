3b411bfe60448d59322807f3b1d26b56c09724e3
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2022-02-03 14:00:06 +0100

    add Pricing Plan and layout to the Pricing page

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,12 +1,82 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import FeaturePricingLayout from "../components/FeaturePricingLayout";
+import PricingPlan from "../components/PricingPlan";
+
+import classes from "../sass/pages/Pricing.module.scss";
+
+import pricingPlanData from "../dev-data/assets/pricing/pricingPlan.json";
 
 const Pricing = () => {
+	const [isYearly, setIsYearly] = useState(false);
+
 	useEffect(() => {
 		document.title = "Photosnap | Pricing";
 		window.scrollTo(0, 0);
 	}, []);
+	console.log(isYearly);
 
-	return <h1>This is the pricing page</h1>;
+	const pricing = pricingPlanData.map((el) => (
+		<PricingPlan
+			key={el.id}
+			title={el.title}
+			body={el.body}
+			amount={el.amount}
+			dark={el.dark}
+			checkDuration={isYearly}
+		/>
+	));
+
+	const switchPlanHandler = () => {
+		setIsYearly(!isYearly);
+	};
+
+	return (
+		<FeaturePricingLayout
+			image={`pricing/mobile/hero.jpg`}
+			title={"Pricing"}
+			content={
+				" Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos."
+			}
+		>
+			<section className={classes["pricing__subscription"]}>
+				<div className={classes["pricing__subscription--plans"]}>
+					<p
+						className={`${
+							isYearly
+								? classes["pricing__subscription--plans__blur"]
+								: classes["pricing__subscription--plans__bold"]
+						} `}
+					>
+						Monthly
+					</p>
+					<div
+						onClick={switchPlanHandler}
+						className={`${classes["pricing__subscription--plans-toggle"]} ${
+							isYearly && classes["pricing__subscription--plans__yearlyBg"]
+						}`}
+					>
+						<div
+							className={`${
+								classes["pricing__subscription--plans-toggle_switch"]
+							} ${isYearly && classes["pricing__subscription--plans__yearly"]}`}
+						></div>
+					</div>
+					<p
+						className={`${
+							!isYearly
+								? classes["pricing__subscription--plans__blur"]
+								: classes["pricing__subscription--plans__bold"]
+						} `}
+					>
+						Yearly
+					</p>
+				</div>
+
+				{pricing}
+			</section>
+		</FeaturePricingLayout>
+	);
 };
 
 export default Pricing;
