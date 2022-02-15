a7b0a07b533a8216bc7fdd671e4eed62ad487a75
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2022-02-15 14:31:07 +0100

    add pricing plan responsive design [Tablet view]

--- src/components/PricingPlan.js
+++ src/components/PricingPlan.js
@@ -1,9 +1,12 @@
 import React from "react";
+import useMediaQuery from "../hooks/useMediaQuery";
 
 import classes from "../sass/components/PricingPlan.module.scss";
 import Button from "./UI/Button";
 
 const PricingPlan = (props) => {
+	const screenTablet = useMediaQuery("(min-width: 768px)");
+
 	//checkDuration is false when it's monthly duration
 	const amount = props.checkDuration ? +props.amount * 10 : +props.amount;
 
@@ -14,16 +17,29 @@ const PricingPlan = (props) => {
 					props.dark ? classes["plans-dark"] : ""
 				}`}
 			>
-				<h3 className={classes["title"]}>{props.title}</h3>
+				<div className={classes["left"]}>
+					<h3 className={classes["title"]}>{props.title}</h3>
 
-				<p className={classes["body"]}>{props.body}</p>
+					<p className={classes["body"]}>{props.body}</p>
 
-				<p className={classes["amount"]}>${amount.toFixed(2)}</p>
-				<p className={classes["period"]}>per month</p>
+					{!screenTablet && (
+						<>
+							<p className={classes["amount"]}>${amount.toFixed(2)}</p>
+							<p className={classes["period"]}>per month</p>
+						</>
+					)}
 
-				<Button className={classes["button"]}>
-					{`Pick plan`.toUpperCase()}
-				</Button>
+					<Button className={classes["button"]}>
+						{`Pick plan`.toUpperCase()}
+					</Button>
+				</div>
+
+				{screenTablet && (
+					<div className={classes["right"]}>
+						<p className={classes["amount"]}>${amount.toFixed(2)}</p>
+						<p className={classes["period"]}>per month</p>
+					</div>
+				)}
 			</div>
 		</>
 	);
